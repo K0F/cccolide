65da4d3c68654f03bcaef2ad25962ff484a45e2a
k0f <krystof.pesek@gmail.com>
2016-03-19 21:18:46 +0100

    WIN32 fix suffix

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,14 @@
 using namespace std;
 using namespace cprocessing;
 
+FILE *fp;
+char buf[BUFSIZE];
+
 int start() {
 
   string cmd = "sclang";
   string arguments = string(" -r -s");
   cmd += arguments;
-
-  char buf[BUFSIZE];
-  FILE *fp;
 
   if ((fp = popen(cmd.c_str(), "r")) == NULL) {
     printf("Error opening pipe!\n");
@@ -46,32 +46,11 @@ void setup() {
   start();
 }
 
-void draw() { background(255); }
+void draw() {
+  background(255);
 
-/*
-int main(int *argc, char *argv[]) {
-  string cmd = "sclang";
-  string arguments = string(" -r -s start.scd");
-  cmd += arguments;
-
-  char buf[BUFSIZE];
-  FILE *fp;
-
-  if ((fp = popen(cmd.c_str(), "r")) == NULL) {
-    printf("Error opening pipe!\n");
-    return -1;
-  }
-
-  while (fgets(buf, BUFSIZE, fp) != NULL) {
+  if (fgets(buf, BUFSIZE, fp) != NULL) {
     // Do whatever you want here...
     printf("OUTPUT: %s", buf);
   }
-
-  if (pclose(fp)) {
-    printf("Command not found or exited with error status\n");
-    return -1;
-  }
-
-  return 0;
 }
-*/
